1e4cc615612726c1be1192e9953ac7deb942ec1d
BernardoWE <bernardo.augusto8022@gmail.com>
2026-08-05 19:46:18 -0300

    adiciona suporte ao MongoDB e cria esquema de pedidos

--- src/database/index.js
+++ src/database/index.js
@@ -3,17 +3,23 @@ import User from '../app/models/user.js';
 import databaseConfig from '../config/database.cjs';
 import Product from '../app/models/product.js';
 import Category from '../app/models/category.js';
-
+import mongoose from 'mongoose';
 const models = [User, Product, Category];
 
 class Database {
   constructor() {
     this.init();
+    this.mongo()
   }
   init() {
     this.connection = new Sequelize(databaseConfig);
     models.map((model) => model.init(this.connection))
     .map((model) => model.associate && model.associate(this.connection.models));
+  }
+  mongo(){
+    this.mongooseConnection = mongoose.connect(
+      'mongodb://localhost:27017/devburguer'
+    )
   }
 }
 
